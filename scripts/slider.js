589ad2f9da5063294c3564a20f7385c436852c71
NotDmitry <91372817+NotDmitry@users.noreply.github.com>
2022-04-11 19:52:37 +0300

    Update slider.js

--- scripts/slider.js
+++ scripts/slider.js
@@ -1,22 +1,42 @@
-let imgIndex = 0;
-let images = [];
-let slideTimer  = 3000;
+/*==========INITIALIZE==============*/
+let imgIndex = 2; // Global counter of images
+if (sessionStorage.getItem("imgIndexSlider") !== null)
+    imgIndex = parseInt(sessionStorage.getItem("imgIndexSlider"));
+let images = []; // Array of urls of images
+const slideTimer  = 7000; // Delay
+const heroPage = document.getElementById("Hero-PageJS"); // Hero Page reference
+const radioBtn = document.getElementsByName("Slider"); // Array of Buttons
+/*==================================*/
 
-images[0] = '/images/HeroFood.png';
+/*=========IMAGES==============*/
+images[0] = '/images/HeroFood1.png';
 images[1] = '/images/HeroFood2.png';
 images[2] = '/images/HeroFood3.png';
 images[3] = '/images/HeroFood4.png';
 images[4] = '/images/HeroFood5.png';
+/*=============================*/
 
+/*==========Set Event=============*/
+for (let i = 0; i < radioBtn.length; i++){
+    radioBtn[i].addEventListener('change', function () {
+            imgIndex = i;
+            sessionStorage.setItem("imgIndexSlider", imgIndex.toString(10));
+            heroPage.style.backgroundImage = "url(" + images[imgIndex] + ")";
+            imgIndex = ++imgIndex % images.length;
+        }
+    );
+}
+/*=================================*/
+
+/*=============Auto Play===============*/
 function changePicture() {
-    let heroPage = document.getElementById("Hero-PageJS")
-    heroPage.style.backgroundImage = "url(" + images[imgIndex] + ")"
 
-    if (imgIndex < images.length - 1) {
-        imgIndex++;
-    } else {
-        imgIndex = 0;
-    }
+    sessionStorage.setItem("imgIndexSlider", imgIndex.toString(10));
+    radioBtn[imgIndex].checked = true;
+    heroPage.style.backgroundImage = "url(" + images[imgIndex] + ")";
+
+    imgIndex = ++imgIndex % images.length;
     setTimeout("changePicture()", slideTimer);
+
 }
 window.onload = changePicture;
