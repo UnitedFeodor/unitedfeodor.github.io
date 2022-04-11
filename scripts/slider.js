5cb5fccdad33098f44cb7f94e8658d5b0350b455
NotDmitry <91372817+NotDmitry@users.noreply.github.com>
2022-04-11 22:18:18 +0300

    Update slider.js

--- scripts/slider.js
+++ scripts/slider.js
@@ -4,16 +4,17 @@ if (sessionStorage.getItem("imgIndexSlider") !== null)
     imgIndex = parseInt(sessionStorage.getItem("imgIndexSlider"));
 let images = []; // Array of urls of images
 const slideTimer  = 7000; // Delay
+let setDelayId;
 const heroPage = document.getElementById("Hero-PageJS"); // Hero Page reference
 const radioBtn = document.getElementsByName("Slider"); // Array of Buttons
 /*==================================*/
 
 /*=========IMAGES==============*/
-images[0] = '../images/HeroFood1.png';
-images[1] = '../images/HeroFood2.png';
-images[2] = '../images/HeroFood3.png';
-images[3] = '../images/HeroFood4.png';
-images[4] = '../images/HeroFood5.png';
+images[0] = '/images/HeroFood1.png';
+images[1] = '/images/HeroFood2.png';
+images[2] = '/images/HeroFood3.png';
+images[3] = '/images/HeroFood4.png';
+images[4] = '/images/HeroFood5.png';
 /*=============================*/
 
 /*==========Set Event=============*/
@@ -22,7 +23,10 @@ for (let i = 0; i < radioBtn.length; i++){
             imgIndex = i;
             sessionStorage.setItem("imgIndexSlider", imgIndex.toString(10));
             heroPage.style.backgroundImage = "url(" + images[imgIndex] + ")";
+            clearTimeout(setDelayId);
+            setDelayId = setTimeout("changePicture()", slideTimer);
             imgIndex = ++imgIndex % images.length;
+
         }
     );
 }
@@ -36,7 +40,7 @@ function changePicture() {
     heroPage.style.backgroundImage = "url(" + images[imgIndex] + ")";
 
     imgIndex = ++imgIndex % images.length;
-    setTimeout("changePicture()", slideTimer);
+    setDelayId = setTimeout("changePicture()", slideTimer);
 
 }
 window.onload = changePicture;
