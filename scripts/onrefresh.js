8a8cd368c4cc738aecdc00ce18c14a46b7bc42fc
NotDmitry <91372817+NotDmitry@users.noreply.github.com>
2022-05-14 13:36:52 +0300

    Update onrefresh.js

--- scripts/onrefresh.js
+++ scripts/onrefresh.js
@@ -1,8 +1,4 @@
-if (sessionStorage.getItem("savebtn") == null)
-	{
-	  sessionStorage.setItem("savebtn", 1);
-	}
-	else
+if (sessionStorage.getItem("savebtn") !== null)
 	{
 		const btn = document.getElementById('savebtn');
 	  btn.innerHTML = '<span id = "savebtn" class="save-button" ><i class="fa-solid fa-check"></i>Saved</span>'
